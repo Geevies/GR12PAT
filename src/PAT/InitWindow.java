3ac4037a7fabf597a658cea8d82fb56fb9435d7d
Geevies <ggrobler447@gmail.com>
2019-06-24 17:39:56 +0200

    init

---
+++ src/PAT/InitWindow.java
@@ -0,0 +1,21 @@
+package PAT;
+
+import javax.swing.JFrame;
+
+public class InitWindow {
+
+	public static void main(String[] args) {
+		JFrame base = new JFrame();
+		base = new JFrame();
+		base.setSize(800, 600);
+		base.setTitle("Data Validation!");
+		base.setVisible(true);
+		base.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		base.setLocationRelativeTo(null);  
+		base.setResizable(true);
+		EmployeeEnter enter = new EmployeeEnter();
+		base.add(enter, 0);
+		base.pack();
+	}
+	
+}
